c244e993edefddc6c57ee8ab71d93f63ed6c880f
Vidya Prabhakaran -X (vidprabh - PERSISTENT SYSTEMS INC at Cisco) <vidprabh@cisco.com>
2020-05-06 17:15:40 -0700

    initial commit

---
+++ src/main/java/guru/springframework/sfgpetclinic1/SfgPetClinic1Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinic1Application.class, args);
+    }
+
+}
